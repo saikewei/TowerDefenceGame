edb11660a03eee72ffef4660a423a43e9a15cd6b
AMD_YES\31830 <3183054890@qq.com>
2023-11-27 21:46:05 +0800

    Revert "create project"
    This reverts commit 0f65bc6801a54518e379eb421a8d5e9e072a3c0e.

--- Source/TowerDefenceGame/TowerDefenceGame.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TowerDefenceGame.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TowerDefenceGame, "TowerDefenceGame" );

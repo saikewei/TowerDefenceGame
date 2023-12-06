e33dd792cd853344849ab2171642c1782099801b
Pikachu231121 <18250670351@163.com>
2023-12-06 21:50:08 +0800

    Add UI for tower building

---
+++ Source/TowerDefenceGame/TBottle.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TowerPaperFlipbookActor.h"
+#include "TBottle.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOWERDEFENCEGAME_API ATBottle : public ATowerPaperFlipbookActor
+{
+	GENERATED_BODY()
+	
+public:
+	ATBottle();
+	//转向目标怪物
+	void RotateToTarget();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+};

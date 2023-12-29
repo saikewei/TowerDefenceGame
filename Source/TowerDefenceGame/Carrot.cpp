250133ea367143128f0ac9e04bca31932b9abab7
wesley <1037257154@qq.com>
2023-12-29 12:44:47 +0800

    Flipbook of carrot change when health changing Add UI for selecting levels

--- Source/TowerDefenceGame/Carrot.cpp
+++ Source/TowerDefenceGame/Carrot.cpp
@@ -14,6 +14,16 @@ ACarrot::ACarrot()
 	//设置血量默认值
 	CarrotHealth = 100.0f;
 
+	Carrot_1 = LoadObject<UPaperFlipbook>(nullptr, TEXT("/Script/Paper2D.PaperFlipbook'/Game/GuardCarrot/Flipbooks/carrot_1.carrot_1'"));
+	Carrot_2 = LoadObject<UPaperFlipbook>(nullptr, TEXT("/Script/Paper2D.PaperFlipbook'/Game/GuardCarrot/Flipbooks/carrot_2.carrot_2'"));
+	Carrot_3 = LoadObject<UPaperFlipbook>(nullptr, TEXT("/Script/Paper2D.PaperFlipbook'/Game/GuardCarrot/Flipbooks/carrot_3.carrot_3'"));
+	Carrot_4 = LoadObject<UPaperFlipbook>(nullptr, TEXT("/Script/Paper2D.PaperFlipbook'/Game/GuardCarrot/Flipbooks/carrot_4.carrot_4'"));
+	Carrot_5 = LoadObject<UPaperFlipbook>(nullptr, TEXT("/Script/Paper2D.PaperFlipbook'/Game/GuardCarrot/Flipbooks/carrot_5.carrot_5'"));
+	FlipbookComponent = CreateDefaultSubobject<UPaperFlipbookComponent>(TEXT("FlipbookComponent"));
+	FlipbookComponent->SetupAttachment(RootComponent);
+	SetRootComponent(FlipbookComponent);
+
+	FlipbookComponent->SetFlipbook(Carrot_1);
 }
 
 // Called when the game starts or when spawned
@@ -30,10 +40,34 @@ void ACarrot::Tick(float DeltaTime)
 
 }
 
+void ACarrot::UpdateFlipbook()
+{
+	UPaperFlipbook* NewFlipbook = nullptr;
+	if (FlipbookComponent)
+	{
+		if (CarrotHealth > 80.0f)
+			NewFlipbook = Carrot_1;
+		// Set flipbook for high health
+		else if (CarrotHealth > 60.0f)
+			NewFlipbook = Carrot_2;
+		// Set flipbook for moderate health
+		else if (CarrotHealth > 40.0f)
+			NewFlipbook = Carrot_3;
+		// Set flipbook for low health
+		else if (CarrotHealth > 20.0f)
+			NewFlipbook = Carrot_4;
+		else
+			NewFlipbook = Carrot_5;
+	}
+	FlipbookComponent->SetFlipbook(NewFlipbook);
+}
+
+
 //获得给萝卜的伤害
 void ACarrot::GetDamage(float Damage)
 {
 	CarrotHealth -= Damage;
+	UpdateFlipbook(); // 更新萝卜图像
 	if (CarrotHealth < 0)
 	{
 		CarrotHealth = 0;
@@ -41,11 +75,10 @@ void ACarrot::GetDamage(float Damage)
 	}
 }
 
-void ACarrot::TakeDamage(float DamageAmount)
+void ACarrot::MyTakeDamage(float DamageAmount)
 {
 	// 减血
 	CarrotHealth -= DamageAmount;
-
 	if (CarrotHealth <= 0.0f)
 	{
 		Destroy();
--- Source/TowerDefenceGame/Carrot.h
+++ Source/TowerDefenceGame/Carrot.h
@@ -3,7 +3,9 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "PaperFlipbook.h"
 #include "PaperFlipbookActor.h"
+#include "PaperFlipbookComponent.h"
 #include "Carrot.generated.h"
 
 UCLASS()
@@ -16,20 +18,40 @@ public:
 	ACarrot();
 
 	UFUNCTION()
-	void TakeDamage(float DamageAmount);
+	void MyTakeDamage(float DamageAmount);
+	void UpdateFlipbook(); //根据萝卜血量更新Flipbook
+
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Flipbook", meta = (AllowPrivateAccess = "true"))
+		UPaperFlipbook* Carrot_1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Flipbook", meta = (AllowPrivateAccess = "true"))
+		UPaperFlipbook* Carrot_2;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Flipbook", meta = (AllowPrivateAccess = "true"))
+		UPaperFlipbook* Carrot_3;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Flipbook", meta = (AllowPrivateAccess = "true"))
+		UPaperFlipbook* Carrot_4;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Flipbook", meta = (AllowPrivateAccess = "true"))
+		UPaperFlipbook* Carrot_5;
+
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Carrot")
 	float CarrotHealth; // 萝卜血量
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Flipbook")
+	class UPaperFlipbookComponent* FlipbookComponent;
 
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 	
 	//获得给萝卜的伤害
-	UFUNCTION(BlueprintCallable, category = "Damage")
+	UFUNCTION(BlueprintCallable, Category = "Damage")
 	void GetDamage(float Damage);
 };
